5bdf64abc13a73c2a92511f27a77c07c2a7fb1b4
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-01-15 10:56:58 +0800

    Solve Applicative.instance Optional

--- src/Course/Applicative.hs
+++ src/Course/Applicative.hs
@@ -93,13 +93,13 @@ instance Applicative Optional where
     a
     -> Optional a
   pure =
-    error "todo: Course.Applicative pure#instance Optional"
+    Full
   (<*>) ::
     Optional (a -> b)
     -> Optional a
     -> Optional b
   (<*>) =
-    error "todo: Course.Apply (<*>)#instance Optional"
+    applyOptional
 
 -- | Insert into a constant function.
 --
