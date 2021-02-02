1a95f68c431bb95a2852de3f5e1dcdb9a2ae7400
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-02-03 07:51:22 +0800

    Solve FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,46 +85,46 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile fp cs =
+  putStrLn fp >>= \_ -> putStrLn cs
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles fpcs =
+  foldLeft (>>) (pure ()) $ (\(fp, cs) -> printFile fp cs) <$> fpcs
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile fp =
+  (,) fp <$> readFile fp
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles fps =
+  sequence $ getFile <$> fps
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@, @lines@, and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run fp =
+ readFile fp >>= pure . lines >>= getFiles >>= printFiles
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
 main =
-  error "todo: Course.FileIO#main"
+   getArgs >>= pure . (map run) >>= foldLeft (>>) (pure ())
 
 ----
 
