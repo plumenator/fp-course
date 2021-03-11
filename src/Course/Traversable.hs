3bb78e491e0ae8c7976e1ad1460a54cc31b42da0
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-03-11 08:58:12 +0800

    Rename for clarity

--- src/Course/Traversable.hs
+++ src/Course/Traversable.hs
@@ -37,7 +37,9 @@ instance Traversable List where
     -> List a
     -> k (List b)
   traverse f =
-    foldRight (\a b -> (:.) <$> f a <*> b) (pure Nil)
+    foldRight (\a kb -> (:.) <$> f a <*> kb) (pure Nil)
+    -- foldRight (lift2 (:.) . f) (pure Nil)
+    -- sequence . (f <$>)
 
 instance Traversable ExactlyOne where
   traverse ::
