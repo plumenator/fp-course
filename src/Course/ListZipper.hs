96e1f06be5f0004e54c90e5cbc9cffc484be8ed9
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-03-31 22:22:47 +0800

    Just use reverse
    reverse uses foldLeft and (++) uses foldRight, so it should be about
    as efficient as this.

--- src/Course/ListZipper.hs
+++ src/Course/ListZipper.hs
@@ -97,7 +97,8 @@ toList (ListZipper ls p rs) =
   -- push ls (p :. rs) where
   -- push Nil ys = ys
   -- push (x :. xs) ys = push xs (x :. ys)
-  foldLeft (flip (:.)) (p :. rs) ls
+  -- foldLeft (flip (:.)) (p :. rs) ls
+  reverse ls ++ (p :. rs)
 
 -- | Convert the given (maybe) zipper back to a list.
 toListZ ::
