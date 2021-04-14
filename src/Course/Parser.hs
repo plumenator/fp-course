11a34a347ec215f8d22a23818930470f56791796
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-04-14 20:20:58 +0800

    Solve Parser.smokerParser

--- src/Course/Parser.hs
+++ src/Course/Parser.hs
@@ -572,7 +572,7 @@ surnameParser =
 smokerParser ::
   Parser Bool
 smokerParser =
-  error "todo: Course.Parser#smokerParser"
+  const True <$> is 'y' ||| const False <$> is 'n'
 
 -- | Write part of a parser for Person#phoneBody.
 -- This parser will only produce a string of digits, dots or hyphens.
